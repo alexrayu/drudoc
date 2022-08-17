a47ac2d5e310611a45fdb41d91f322bf7a5b37cb
Alexei Raiu <alex.rayu@gmail.com>
2022-08-17 21:39:58 +0300

    Initial app.

--- main.php
+++ main.php
@@ -5,6 +5,19 @@
  * The main PHP file.
  */
 
-require_once 'vendor/autoload.php';
-require_once 'settings.php';
+require_once './vendor/autoload.php';
+require_once './settings.php';
 
+
+require '../../vendor/autoload.php';
+require 'CreateDocsCommand.php';
+
+use Symfony\Component\Console\Application;
+use application\CreateDocsCommand;
+
+$app = new Application('drudoc');
+$app->add(new CreateDocsCommand());
+try {
+  $app->run();
+} catch (\Exception $e) {
+}
--- settings.php
+++ settings.php
@@ -5,3 +5,7 @@
  * The settings file.
  */
 
+define('DRUDOC_ROOT', __DIR__);
+define('DRUDOC_SRC', '');
+define('DRUDOC_DST', DRUDOC_ROOT . '/output/doc.pdf');
+error_reporting(0);
