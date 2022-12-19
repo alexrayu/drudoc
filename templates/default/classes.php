aff760e81b8bd6334ce7d1eda2e7851da6e2ad13
Alexei Raiu <alex.rayu@gmail.com>
2022-12-19 21:54:12 +0200

    Initial working reflection.

--- templates/default/classes.php
+++ templates/default/classes.php
@@ -5,8 +5,8 @@
 
 
 <?php $i++; ?>
-### <?php echo $i; ?>. <?php _d($class_info['name']); ?>
-  - **Namespace:** `<?php echo $class_info['namespace']; ?>`
+### <?php echo $i; ?>. <?php echo _d($class_info['name']); ?>
+  - **Namespace:** `<?php echo $class_info['namespace']; ?>`.
 <?php if (!empty($class_info['summary'])) { ?>
   - **Summary:** <?php echo _d($class_info['summary']); ?>
 <?php } ?>
